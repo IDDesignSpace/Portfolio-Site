64dd58e30babc34e5064c548ad59fe29beda2b58
IDDesignSpace <42154502+IDDesignSpace@users.noreply.github.com>
2019-04-30 12:13:43 -0400

    added NavBar and Main

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const NavBar = function() {
+    return (
+        <div className=""> 
+            <div className="">
+                <h2>idDesignSpace</h2>
+            </div>
+            <div className="">
+                <a>Home</a>
+                <a>About</a>
+                <a>Contact</a>
+            </div>            
+        </div>
+    );
+};
+class Main extends React.Component {
+    constructor(props) {
+        super(props); 
+    }
+    render() {
+        return (
+            
+        );
+
+    }
+}
+
+class MyPage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <NavBar />        
+            </div>
+        );
+    }
+};
+
+
+
+
+ReactDOM.render(<MyPage />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
