da1745a21890e453d025a225659525a2af6f091b
IDDesignSpace <42154502+IDDesignSpace@users.noreply.github.com>
2019-05-12 12:41:59 -0400

    Working on About information

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import Popper from 'popper.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/styles.scss';
-import logo from './img/id.png'
+import logo from './img/id.png';
+
+{/* Navbar Component */ }
 
 const NavBar = function() {
     return (
+        <div className="row nav-bar">
         <div class="container-fluid">
-            <div className="row nav-bar">
                 <div className="col-lg-9  col-md-6 nav">
-                    <img src={logo} className="logo-nav img-responsive" />
+                    <img src={logo} alt="iddespignspace-logo" className="logo-nav img-responsive" />
                     <h2>idDesignSpace</h2>
                 </div>
                 <div className="col-lg-3 col-md-6 nav-links">
@@ -27,6 +29,7 @@ const NavBar = function() {
     );
 };
 
+{/* Main Component */ }
 class Main extends React.Component {
     constructor(props) {
         super(props); 
@@ -37,6 +40,7 @@ class Main extends React.Component {
             
                 <img src={logo} className="logo"/>
                 <h1>Welcome to idDesignSpace</h1>
+                <p>Hello,<br /> My name is Ian Barkan and this is idDesignSpace. This is a place to showcase my skills as a Front end web developer.</p>
              </div>
            
         );
@@ -44,6 +48,7 @@ class Main extends React.Component {
     }
 }
 
+{/* About Component */ }
 class About extends React.Component {
     constructor(props) {
         super(props);
@@ -53,13 +58,14 @@ class About extends React.Component {
             <div>
                 <h2>About</h2>
                 <img src=""/>
-                <p></p>
+                <p>I love to build and learn new things. I love being challenged.  </p>
 
             </div>
         );
     }
 }
 
+{/* Skills Component */ }
 class Skills extends React.Component {
     constructor(props) {
         super(props);
@@ -76,6 +82,7 @@ class Skills extends React.Component {
     }
 }
 
+{/* Work Component */ }
 class Work extends React.Component {
     constructor(props) {
         super(props);
@@ -90,6 +97,8 @@ class Work extends React.Component {
     }
 }
 
+{/* Contact Component */ }
+
 class Contact extends React.Component {
     constructor(props) {
         super(props);
@@ -103,7 +112,7 @@ class Contact extends React.Component {
     }
 }
 
-class Footer extend React.Component {
+class Footer extends React.Component {
     constructor(props) {
         super(props);
     }
